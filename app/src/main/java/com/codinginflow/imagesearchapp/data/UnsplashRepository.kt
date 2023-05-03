46a51fb63794e5d0d49633a30e9b7db6897107c3
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-05-03 13:37:44 +0300

    Setup dependency injection, and create UnsplashRepository and GalleryViewModel

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
@@ -0,0 +1,9 @@
+package com.codinginflow.imagesearchapp.data
+
+import com.codinginflow.imagesearchapp.api.UnsplashApi
+import javax.inject.Inject
+
+class UnsplashRepository @Inject constructor(
+    private val unsplashApi: UnsplashApi
+) {
+}
--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -1,8 +1,12 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.codinginflow.imagesearchapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
+    private val viewModel: GalleryViewModel by viewModels()
 }
